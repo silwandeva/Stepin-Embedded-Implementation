34ee227f5c50776eb7c5b23a72d72cef2bd61cac
deva silwan <62930320+silwandeva@users.noreply.github.com>
2021-09-16 17:21:26 +0530

    create

--- 3_Implementation/inc/Activity_2.h
+++ 3_Implementation/inc/Activity_2.h
@@ -1,1 +1,25 @@
+/**
+ * @file Activity2.h
+ * @author devasilwan 
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-16 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __ACTIVITY2_H_
+#define __ACTIVITY2_H_
+
+
+
+
+
+#include <avr/io.h>
+#include<util/delay.h>
+#include <avr/interrupt.h>
+
+void InitADC();
+
+uint16_t ReadADC(uint8_t ch);
+#endif /** __Activity2_H_ */
 
