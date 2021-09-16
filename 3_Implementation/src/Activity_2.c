72939c9261ab315a1949e5dbc7fde4bb928f3396
deva silwan <62930320+silwandeva@users.noreply.github.com>
2021-09-16 17:28:40 +0530

    create

--- 3_Implementation/src/Activity_2.c
+++ 3_Implementation/src/Activity_2.c
@@ -1,1 +1,28 @@
+/**
+ * @file Activity_2.c
+ * @author devasilwan
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-16
+ * 
+ * @copyright Copyright (c) 2021.
+ * 
+ */
+#include "Activity_1.h"
+#include "Activity_2.h"
+void InitADC()
+{
+    ADMUX=(1<<REFS0);
+    ADCSRA=(1<<ADEN) | (7<<ADPS0);
+}
 
+uint16_t ReadADC(uint8_t ch)
+{
+    ADMUX&=0xf8;
+    ch=ch&0b00000111;
+    ADMUX|=ch;
+    ADCSRA|=(1<<ADSC);
+    while(!(ADCSRA & (1<<ADIF)));
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+}
